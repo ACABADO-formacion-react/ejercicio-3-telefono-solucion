8194ca777644855723cdd3add03c37779455f026
Mario <mario@mariogl.com>
2021-01-14 17:37:58 +0100

    Ejercicio resuelto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const teclas = [...[...Array(10)].map((x, i) => i).slice(1), 0];
+  const [telefono, setTelefono] = useState("");
+  const [llamando, setLlamando] = useState(false);
+  let timer;
+  const esCompleto = () => telefono.length === 9;
+  const anyadeDigito = (digito) => {
+    if (esCompleto()) {
+      return;
+    }
+    setTelefono(`${telefono}${digito}`);
+  }
+  const borrar = () => {
+    setTelefono("");
+  }
+  const llamar = (e) => {
+    e.preventDefault();
+    setLlamando(true);
+    timer = setTimeout(() => { console.log("Cuelgo"); colgar() }, 5000);
+  }
+  const colgar = (e) => {
+    if (typeof e !== "undefined") {
+      e.preventDefault();
+    }
+    clearTimeout(timer);
+    setLlamando(false);
+    borrar();
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="contenedor">
+      <span className={`mensaje${llamando ? "" : " off"}`}>Llamando...</span>
+      <main className="telefono">
+        <div className="botones">
+          <ol className="teclado">
+            {
+              teclas.map(tecla => (
+                <li key={tecla} onClick={() => anyadeDigito(tecla)}>
+                  <button disabled={llamando}>{tecla}</button>
+                </li>
+              ))
+            }
+            <li>
+              <button className="big" onClick={borrar} disabled={llamando}>borrar</button>
+            </li>
+          </ol>
+        </div>
+        <div className="acciones">
+          <span className="numero">{telefono}</span>
+          {!llamando && <a href="llamar" onClick={llamar} className={`llamar${esCompleto() ? " activo" : ""}`}>Llamar</a>}
+          {llamando && <a href="colgar" onClick={colgar} className="colgar activo">Colgar</a>}
+        </div>
+      </main>
     </div>
   );
 }
